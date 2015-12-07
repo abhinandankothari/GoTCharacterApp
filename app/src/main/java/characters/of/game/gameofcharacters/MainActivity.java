7c44ac23bdc22cc1c4aa735f06ccc0e88b794620
Abhinandan Kothari <abhinandankothari@gmail.com>
2015-12-07 10:51:35 +0530

    Menu Item added

--- app/src/main/java/characters/of/game/gameofcharacters/MainActivity.java
+++ app/src/main/java/characters/of/game/gameofcharacters/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -33,5 +36,24 @@ public class MainActivity extends AppCompatActivity {
         DbHelper dbHelper = new DbHelper(this);
         int rowCount = dbHelper.getRowCount();
         Log.d(DetailActivity.LOG_TAG, "Total rows " + rowCount);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.new_add:
+                Intent intent = new Intent(this, RoleActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
